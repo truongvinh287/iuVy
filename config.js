35a304d70062d9cd91f3d1aaae68475526dfc2d3
truongvinh287 <92418341+truongvinh287@users.noreply.github.com>
2022-10-20 16:24:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello Tường Vy iu zấu!",
+  introDesc: `Nhân dịp lễ như vầy \n
+    Anh có một món quà muốn tặng cho em\n
+    Hãy nhận lấyyyyy :>`,
+  btnIntro: "hihi",
+  title: "Sau đây là màng bốc thăm trúng thưởng :>",
+  desc: "Tường Vy có iu anh hok ",
+  btnYes: "Vẫn cứ là iu anh <33",
+  btnNo: "Không, anh bị đin à :>",
+  question: `Gòi sao Tường Vy lại iu anh zọ <3  \n Muốn nói gì thì gửi cho anhhhh`,
+  btnReply: "Gửi cho anh :>",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Sời không cần nói anh cũng biếttt :>",  
+  messDesc: `Chúc mừng em đã nhận được một món quà :>\n
+             Đó là một cái thơm má từ anh `,
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/profile.php?id=100008433726442", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};  
